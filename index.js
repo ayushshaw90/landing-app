ca44f2b54921f4ffc2a0df310ae8a53b5be7216c
Ayush Shaw <ayushshaw127@gmail.com>
2022-04-30 15:03:50 +0530

    revert back

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const menu = document.getElementById("nav-menu")
 const nav_list = document.getElementById("nav-list")
-// const empty_space = document.getElementById("empty-space")
+const empty_space = document.getElementById("empty-space")
 let show_navlist=false;
 function menuInteract(){
     if(show_navlist){
         show_navlist=false;
         nav_list.classList.add("hide-nav")
         nav_list.classList.remove("show-nav")
-        // empty_space.style.height="40px";
+        empty_space.style.height="40px";
     }else{
         show_navlist=true;
         nav_list.classList.remove("hide-nav")
         nav_list.classList.add("show-nav")
-        // empty_space.style.height="170px";
+        empty_space.style.height="170px";
     }
 }
 menu.addEventListener("click", menuInteract)
